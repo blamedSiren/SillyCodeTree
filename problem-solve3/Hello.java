bc43be8456345f5a6aea821eadb8288392a07340
blamedSiren <lunablossom@Lunas-MacBook-Air.local>
2026-01-27 10:03:55 -0500

    add problem-solve3

---
+++ problem-solve3/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+  public static void main(String[] args) {
+    String name = "Luna";
+    System.out.println("Hello, World!");
+    System.out.println("Welcome to the class, " + name + "!");
+  } //end main
+}//end class
